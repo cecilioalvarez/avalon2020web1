e4063ce8d9a0e16b7d9be56c0cc140bfcd7d8117
Oliver <Oliver@MSI>
2020-03-26 13:59:53 +0100

    26 de marzo

---
+++ avalon2020web1/src/es/avalon/ServletInsertarLibro.java
@@ -0,0 +1,31 @@
+package es.avalon;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.avalon.dominio.Libro;
+
+/**
+ * Servlet implementation class ServletInsertarLibro
+ */
+@WebServlet("/ServletInsertarLibro")
+public class ServletInsertarLibro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+
+		
+
+		
+	}
+
+}
