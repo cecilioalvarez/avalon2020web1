cbfa6fa5bac1f3f6722faf7e7875d79ce43fcf06
Formacion <Formacion@192.168.1.201>
2020-03-26 09:54:47 +0100

    respuesta

--- avalon2020web1/src/es/avalon/Seleccion.java
+++ avalon2020web1/src/es/avalon/Seleccion.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Seleccion
  */
-@WebServlet("/Seleccion")
+@WebServlet("/ServletSeleccion")
 public class Seleccion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
