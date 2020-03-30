51b86f59b21cbb76ce940e909a6c5daf7f4e7fe8
Formacion <Formacion@5CD9494ZLTC>
2020-03-30 13:05:30 +0200

    servlet

---
+++ avalon2020web1/WebContent/libros2/ListaLibros.jsp
@@ -0,0 +1,43 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="es.avalon.dominio.Libro"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%
+	ArrayList<Libro> lista = new ArrayList<Libro>();
+lista = (ArrayList<Libro>) request.getAttribute("ListaLibros");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>ISBN</th>
+			<th>titulo</th>
+			<th>autor</th>
+			<th>precio</th>
+			<th>categoria</th>
+		</tr>
+		<%
+			for (Libro l : lista) {
+		%>
+		<tr>
+			<td><%=l.getIsbn()%></td>
+			<td><%=l.getTitulo()%></td>
+			<td><%=l.getAutor()%></td>
+
+			<td><a href="Borrar.jsp?isbn=<%=l.getIsbn()%>">borrar</a></td>		
+			<td><a href="VerDetalle.jsp?isbn=<%=l.getIsbn()%>">detalle</a></td>		
+			<td><a href="Editar.jsp?isbn=<%=l.getIsbn()%>">editar</a></td>		
+		</tr>
+		<%
+			}
+		%>
+	</table>
+	<a href="formularioInsertar.jsp">insertar</a>
+
+</body>
+</html>
---
+++ avalon2020web1/src/es/avalon/controladores/ServletLibros.java
@@ -0,0 +1,42 @@
+package es.avalon.controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.avalon.dominio.Libro;
+
+/**
+ * Servlet implementation class Libro
+ */
+@WebServlet("/ServletLibros")
+public class ServletLibros extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<Libro> listaLibros=new ArrayList<Libro>();
+		
+		listaLibros=Libro.buscarTodos();
+		
+		//Crear el despachador y decir a donde vamos
+		RequestDispatcher despachador=request.getRequestDispatcher("Libros2/listaLibros.jsp");	
+		//Asignar los datos adicionales
+		request.setAttribute("listaLibros", listaLibros);
+		//Reenvio
+		despachador.forward(request, response);
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
