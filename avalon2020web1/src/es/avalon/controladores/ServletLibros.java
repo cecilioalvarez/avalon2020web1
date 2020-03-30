58ae2c2de4660fd65ab33a292530a2908af2b7c1
Razvan Pascal <razvanpascal@outlook.com>
2020-03-30 13:22:50 +0200

    controladores

---
+++ avalon2020web1/src/es/avalon/controladores/ServletLibros.java
@@ -0,0 +1,41 @@
+package es.avalon.controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.avalon.dominio.Libro;
+
+
+@WebServlet("/ServletLibros")
+public class ServletLibros extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ArrayList<Libro> listaLibros = new ArrayList<Libro>();
+		
+		listaLibros = Libro.buscarTodos();
+		
+		RequestDispatcher despachador = request.getRequestDispatcher("libros2/ListaLibros.jsp");
+		
+		request.setAttribute("listaLibros", listaLibros);
+		
+		despachador.forward(request, response);
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
