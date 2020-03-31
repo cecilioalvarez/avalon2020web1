f8e26c912523bbcefcf4c23ba9a9b8d03e08dd56
Formacion <Formacion@5CD9494ZLTC>
2020-03-31 11:21:12 +0200

    error noinserta

--- avalon2020web1/src/es/avalon/controladores/ServletLibros.java
+++ avalon2020web1/src/es/avalon/controladores/ServletLibros.java
@@ -18,34 +18,54 @@ import es.avalon.dominio.Libro;
  */
 @WebServlet("/ServletLibros")
 public class ServletLibros extends HttpServlet {
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		RequestDispatcher despachador=null;	
-		String accion=request.getParameter("accion");
-		
-		if (accion!=null) {
-			
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher despachador = null;
+		String accion = request.getParameter("accion");
+
+		if (accion != null) {
+
 			if (accion.equals("formularioInsertar")) {
-				despachador=request.getRequestDispatcher("libros2/formularioInsertar.jsp");
+				despachador = request.getRequestDispatcher("libros2/formularioInsertar.jsp");
+			}
+			// Accion=insertar
+			else {
+				// Recepcionar
+				String isbn = request.getParameter("isbn");
+				String titulo = request.getParameter("titulo");
+				String autor = request.getParameter("autor");
+				int precio = Integer.parseInt(request.getParameter("precio"));
+				String categoria = request.getParameter("categoria");
+				// Insertar
+				Libro milibro = new Libro(isbn, titulo, autor, precio, categoria);
+				milibro.insertar();
+				// Cargar nuevo listado
+				List<Libro> listaLibros = Libro.buscarTodos();
+				request.setAttribute("listaLibros", listaLibros);
+				// Redirigir
+				despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
 			}
 		}
+
+		// no hay acion
 		else {
-		
-		List<Libro> listaLibros=new ArrayList<Libro>();
-		
-		listaLibros=Libro.buscarTodos();
-		
-		despachador=request.getRequestDispatcher("libros2/listaLibros.jsp");
-		request.setAttribute("listaLibros", listaLibros);
+
+			List<Libro> listaLibros = new ArrayList<Libro>();
+
+			listaLibros = Libro.buscarTodos();
+
+			despachador = request.getRequestDispatcher("libros2/listaLibros.jsp");
+			request.setAttribute("listaLibros", listaLibros);
 		}
-		//Sean de un interface
+		// Sean de un interface
 		despachador.forward(request, response);
 
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
